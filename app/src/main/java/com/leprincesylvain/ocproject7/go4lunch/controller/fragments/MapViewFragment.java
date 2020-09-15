c82da9a2f316c6d0f69c8824531e52c302ab15f3
boco0711 <s.lep.0711@gmail.com>
2020-09-15 10:54:08 +0200

    Début d'initialisation de la GoogleMap

--- app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/fragments/MapViewFragment.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/fragments/MapViewFragment.java
@@ -1,4 +1,31 @@
 package com.leprincesylvain.ocproject7.go4lunch.controller.fragments;
 
-public class MapViewFragment {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+    public static final String TAG = "MapViewFragment_TAG";
+
+    private GoogleMap googleMap;
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: ");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: ");
+        this.googleMap = googleMap;
+    }
 }
