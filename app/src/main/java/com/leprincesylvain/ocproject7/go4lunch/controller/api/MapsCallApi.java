251cb08811d37d0d12f9eaa5bedb2e9daa389d26
boco0711 <s.lep.0711@gmail.com>
2020-09-15 20:09:29 +0200

    @Get Call<ResponseToPlace>

--- app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/api/MapsCallApi.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/api/MapsCallApi.java
@@ -1,5 +1,12 @@
 package com.leprincesylvain.ocproject7.go4lunch.controller.api;
 
+import com.leprincesylvain.ocproject7.go4lunch.model.ResponseToPlace;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
 public interface MapsCallApi {
 
+    @GET
+    Call<ResponseToPlace> getListOfRestaurants(@Url String url);
 }
