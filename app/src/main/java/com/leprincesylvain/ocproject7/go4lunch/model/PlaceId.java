b6ab63e964767c99bffbdf5ab53cd303c97bde30
boco0711 <s.lep.0711@gmail.com>
2020-09-15 19:01:40 +0200

    ResponseToPlace et PlaceId

---
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/model/PlaceId.java
@@ -0,0 +1,12 @@
+package com.leprincesylvain.ocproject7.go4lunch.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PlaceId {
+    @SerializedName("place_id")
+    private String placeId;
+
+    public String getPlaceId() {
+        return placeId;
+    }
+}
---
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/model/ResponseToPlace.java
@@ -0,0 +1,44 @@
+package com.leprincesylvain.ocproject7.go4lunch.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseToPlace {
+    @SerializedName("html_attributions")
+    @Expose
+    private List<Object> html_attributions = new ArrayList<Object>();
+
+    @SerializedName("results")
+    @Expose
+    private List<PlaceId> results = new ArrayList<PlaceId>();
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public List<Object> getHtml_attributions() {
+        return html_attributions;
+    }
+
+    public void setHtml_attributions(List<Object> html_attributions) {
+        this.html_attributions = html_attributions;
+    }
+
+    public List<PlaceId> getResults() {
+        return results;
+    }
+
+    public void setResults(List<PlaceId> results) {
+        this.results = results;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
