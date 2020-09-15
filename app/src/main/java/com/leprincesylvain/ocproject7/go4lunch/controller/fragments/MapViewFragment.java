89b8fb1244616fa1ae9d53995ec1ed06973566ab
boco0711 <s.lep.0711@gmail.com>
2020-09-15 12:23:27 +0200

    Initialisation de la map basique

--- app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/fragments/MapViewFragment.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/controller/fragments/MapViewFragment.java
@@ -1,5 +1,6 @@
 package com.leprincesylvain.ocproject7.go4lunch.controller.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,17 +11,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.leprincesylvain.ocproject7.go4lunch.R;
 
 public class MapViewFragment extends Fragment implements OnMapReadyCallback {
     public static final String TAG = "MapViewFragment_TAG";
 
     private GoogleMap googleMap;
-    
+    private Context mContext;
+
+    public MapViewFragment(){
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        if (getArguments() != null) {
+            Log.d(TAG, "onCreate: getArguments() != null");
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: ");
-        return super.onCreateView(inflater, container, savedInstanceState);
+        mContext = this.getContext();
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
     @Override
