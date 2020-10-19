fdd1c1116996a62643577067ae7dc8e308a3d54f
boco0711 <s.lep.0711@gmail.com>
2020-10-19 16:29:49 +0200

    Workmate model, field, detter and parcelable

--- app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/model/Workmate.java
+++ app/src/main/java/com/leprincesylvain/ocproject7/go4lunch/model/Workmate.java
@@ -1,4 +1,72 @@
 package com.leprincesylvain.ocproject7.go4lunch.model;
 
-public class Workmate {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Workmate implements Parcelable {
+    private String workmateId;
+    private String userProfilePicture;
+    private String userName;
+    private String userMail;
+    private String restaurantChoice;
+    private int dateOfChoice;
+
+    public Workmate() {
+    }
+
+    public Workmate(String userProfilePicture, String userName, String userMail) {
+        this.userProfilePicture = userProfilePicture;
+        this.userName = userName;
+        this.userMail = userMail;
+    }
+
+    public static final Creator<Workmate> CREATOR = new Creator<Workmate>() {
+        @Override
+        public Workmate createFromParcel(Parcel source) {
+            return new Workmate(source);
+        }
+
+        @Override
+        public Workmate[] newArray(int size) {
+            return new Workmate[size];
+        }
+    };
+
+    public String getUserProfilePicture() {
+        return userProfilePicture;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public String getRestaurantChoice() {
+        return restaurantChoice;
+    }
+
+    public int getDateOfChoice() {
+        return dateOfChoice;
+    }
+
+    protected Workmate(Parcel in) {
+        userProfilePicture = in.readString();
+        userName = in.readString();
+        userMail = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(userProfilePicture);
+        dest.writeString(userMail);
+        dest.writeString(userName);
+    }
 }
